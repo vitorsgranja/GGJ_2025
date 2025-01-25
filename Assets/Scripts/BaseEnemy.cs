5ba7fac7ba65ec31f11086de689827a69bcb53e3
Danny <dannydmms@gmail.com>
2025-01-25 11:57:31 -0300

    Script base dos inimigos
    Script base dos inimigos criado com função de andar para um lado e para o outro, mudando de direção quando acha parede ou buraco

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+
+    [Header("Enemy stats")]
+    [SerializeField] private float life;
+    [SerializeField] private float speed;
+
+    [Space(10)]
+
+    [Header("Check variables")]
+    [SerializeField] private float hitWallDistance;
+    [SerializeField] private float hitHoleDistance;
+    [SerializeField] private float hitHoleDistanceFromPlayer;
+    [SerializeField] private Rigidbody2D enemyRB;
+    [SerializeField] private LayerMask wallLayer;
+
+    private bool moveRight = true;
+
+    private void Update()
+    {
+        enemyRB.linearVelocity = new Vector2(speed, 0);
+    }
+
+    private void TurnAroundHitWall()
+    {
+        if (moveRight)
+        {
+            RaycastHit2D wallInFront = Physics2D.Raycast(this.transform.position, Vector2.right, hitWallDistance, wallLayer);
+            Debug.DrawRay(this.transform.position, Vector2.right, Color.red);
+
+            if (wallInFront.collider != null)
+            {
+                moveRight = false;
+                speed *= -1;
+            }
+        }
+        else
+        {
+            RaycastHit2D wallInFront = Physics2D.Raycast(this.transform.position, Vector2.left, hitWallDistance, wallLayer);
+            Debug.DrawRay(this.transform.position, Vector2.left, Color.red);
+            if (wallInFront.collider != null)
+            {
+                moveRight = true;
+                speed *= -1;
+            }
+        }
+    }
+
+    private void TurnAroundDetectHole()
+    {
+        if (moveRight)
+        {
+            RaycastHit2D holeInFront = Physics2D.Raycast(new Vector2(this.transform.position.x + hitHoleDistanceFromPlayer, this.transform.position.y), -Vector2.up, hitHoleDistance, wallLayer);
+            Debug.DrawRay(new Vector2(transform.position.x + hitHoleDistanceFromPlayer, this.transform.position.y), Vector2.down, Color.red);
+            if (holeInFront.collider == null)
+            {
+                moveRight = false;
+                speed *= -1;
+            }
+        }
+        else
+        {
+            RaycastHit2D holeInFront = Physics2D.Raycast(new Vector2(this.transform.position.x - hitHoleDistanceFromPlayer, this.transform.position.y), -Vector2.up, hitHoleDistance, wallLayer);
+            Debug.DrawRay(new Vector2(transform.position.x - hitHoleDistanceFromPlayer, this.transform.position.y), Vector2.down, Color.red);
+            if (holeInFront.collider == null)
+            {
+                moveRight = true;
+                speed *= -1;
+            }
+        }
+    }
+}
