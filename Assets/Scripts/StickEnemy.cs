2451bed93b1f1f6acd66df55fefd1a7a17ed0ed1
Danny <dannydmms@gmail.com>
2025-01-25 19:08:12 -0300

    Adicionando a layer de chão
    criei a layer de chão para testar algumas coisas de movimentação

--- Assets/Scripts/StickEnemy.cs
+++ Assets/Scripts/StickEnemy.cs
@@ -18,6 +18,7 @@ public class StickEnemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
